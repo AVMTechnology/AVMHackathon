a201e24a8be98f0086255ffd5712c6cb62412a19
AVMTechnology <AVMTechnology@gmail.com>
2020-09-13 14:24:58 +0300

    Финальная версия VR + добавлен PHP

---
+++ PHP/SetScore.php
@@ -0,0 +1,33 @@
+<?php
+header("Access-Control-Allow-Origin: *");
+$servername = "localhost";
+$database   = "a0193618_hackathon_deti";
+$usernameBD = "a0193618_hackathon_deti";
+$passwordBD = "1234user";
+$usernameUE = $_REQUEST["Username"];
+$highscoreUE = $_REQUEST["Score"];
+
+$conn = mysqli_connect($servername, $usernameBD, $passwordBD, $database) or die("Connection failed");
+
+$loginSQL    = "SELECT Username FROM Users WHERE Username = '$usernameUE'";
+$loginResult = $conn->query($loginSQL);
+
+while ($row = $loginResult->fetch_assoc()) {
+    $login = $row["Username"];
+}
+
+
+if (empty($login)) {
+    
+    $JsonErr=array('err' => 'User do not exist');
+    echo json_encode ($JsonErr);
+    //echo ($registerResult);
+}else{
+    $updateSQL = "UPDATE Users SET Score = '$highscoreUE' WHERE Username = '$usernameUE'";
+    $updateResult = $conn->query($updateSQL);
+    $JsonArray=array('response' => $updateResult);
+    echo json_encode ($JsonArray);
+
+}
+mysqli_close($conn);
+?>
